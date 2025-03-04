3876868469ff832f149b766f04f6f7fdf8cd7a45
Masayuki <equaaqua@hotmail.com>
2025-03-04 19:01:12 +0000

    check permission of storage directory

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,5 +3,6 @@
 cd /workspace/app/laravel &&\
     composer install &&\
     cp ../../.env . &&\
+    chown -R www-data:www-data storage &&\
     php artisan key:generate &&\
     php artisan migrate --force
